061d84e7f844397cca6cb41a062479c4beefe0d5
Takashi Ikki <takahashi@omicale.com>
2020-05-06 21:55:56 +0900

    lartn state code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react';
 import './App.css';
 
 const App = () => {
-    const profiles = [
-        {name: 'Taro', age: 10},
-        {name: 'Hanako', age: 20},
-        {name: 'NoName',},
-    ];
     return (
-        <div>
-            {
-                profiles.map((profile, index) => {
-                    return <User name={profile.name} age={profile.age} key={index}/>
-                })
-            }
-        </div>
+        <Counter>
+        </Counter>
     )
 };
 
-const User = (props) => {
-        return <div>Hi! I am {props.name}!! and {props.age} year old! </div>
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
     }
-;
 
-User.propTypes = {
-    name: PropTypes.string,
-    age: PropTypes.number.isRequired
-};
+    handlePlusButton = () => {
+        this.setState({count: this.state.count + 1})
+    };
 
-User.defaultProps = {
-    age: 1
+    handleMinusButton = () => {
+        this.setState({count: this.state.count - 1})
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>count: {this.state.count}</div>
+                <button onClick={this.handlePlusButton}>+1</button>
+                <button onClick={this.handleMinusButton}>-1</button>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
