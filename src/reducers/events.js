24791fded9c7b694821167b3448d908129814fc7
Takashi Ikki <takahashi@omicale.com>
2020-05-07 23:03:25 +0900

    Event index

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,10 @@
+import _ from 'lodash';
 import {READ_EVENTS} from "../actions";
 
-export default (state = {}, action) => {
-    switch (action.type) {
-        case READ_EVENTS:
-            return state;
-        default:
-            return state
+export default (events = {}, action) => {
+    if (action.type === READ_EVENTS) {
+        return _.mapKeys(action.response.data, 'id');
+    } else {
+        return events
     }
 }
